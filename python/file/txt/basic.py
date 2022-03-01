cab4e11e896f986bf0628387093288a98ef3e409
NguyenThinhDatSci <thinh.nv194178@sis.hust.edu.vn>
2022-03-01 18:27:11 +0700

    ...

---
+++ python/file/txt/basic.py
@@ -0,0 +1,70 @@
+'''
+    open(file)
+    ...
+    close() ~ operating system
+'''
+# help(open)
+# read file
+file = open('test.txt') # file is an iterable
+'''
+    mode = 
+    r  -> only read - Default (file have to exist)
+    r+ -> read and write      (file have to exist)
+    w  -> delete the content of current file + write
+    w+ -> w and r+
+    a  -> write
+    a+ -> write and read
+'''
+content = file.read() # if argument is empty or <0 => read all of lines in this file
+
+file.seek(0) # move the pointer to the head of file
+content = file.read(3)
+
+file.seek(4)
+content = file.read(1000)
+
+file.seek(0)
+content = file.readline()
+
+file.seek(0)
+content = file.readlines()
+# constructor receive iterable
+content = list(file)
+
+print(content)
+
+file.close()
+
+# write file
+# Each time using write. The file pointer will be placed immediately after the last character written.
+# mode = 'w'
+file = open('writeTest.txt', mode = 'w')
+
+file.write('First line')
+file.write('\n\n')
+file.write('Second line')
+file.write('\n\n')
+
+file.close()
+
+# mode = 'a'
+file = open('writeTest.txt', mode = 'a')
+
+file.write('Third line')
+
+file.close()
+
+'''
+with expression [as variable]:
+    with-block
+=> At the end of with-block. File will be closed
+'''
+# way 01
+with open('test.txt') as file:
+    data = file.read()
+
+# way 02
+with (file := open('test.txt')):
+    data = file.read()
+
+print(data)
---
+++ python/file/xlsx/basic.py
@@ -0,0 +1,3 @@
+import openpyxl
+
+getData(fileName)
