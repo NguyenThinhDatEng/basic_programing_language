0898f4ee60dbdc9617afdf55cd5a1fd634b3b77b
NguyenThinhDatEng <toithienamxinnghe@gmail.com>
2022-06-11 10:50:10 +0700

    Done chapter 2

---
+++ C++/Thanh thao C++ qua 108 bai hoc/Chuong_2/Bai 17/01.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    char c;
+    cin >> c;
+    cout << (int)c << endl
+         << (char)(c + 1);
+    return 0;
+}
