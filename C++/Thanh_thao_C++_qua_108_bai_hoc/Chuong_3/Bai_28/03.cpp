33bab28ba1d5925a82294c2822d995d3edd5a1d2
NguyenThinhDatEng <toithienamxinnghe@gmail.com>
2022-06-11 17:16:05 +0700

    accomplish chapter 5

--- C++/Thanh_thao_C++_qua_108_bai_hoc/Chuong_3/Bai_28/03.cpp
+++ C++/Thanh_thao_C++_qua_108_bai_hoc/Chuong_3/Bai_28/03.cpp
@@ -4,118 +4,114 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     int n; // n is a 2 digit number
-    // cin >> n;
-    for (n = 1; n <= 99; n++)
+    cin >> n;
+    if (n / 10)
     {
-        if (n / 10)
+        // tens
+        switch (n / 10)
         {
-            // tens
-            switch (n / 10)
-            {
-            case 2:
-                cout << "Hai";
-                break;
-            case 3:
-                cout << "Ba";
-                break;
-            case 4:
-                cout << "Bon";
-                break;
-            case 5:
-                cout << "Nam";
-                break;
-            case 6:
-                cout << "Sau";
-                break;
-            case 7:
-                cout << "Bay";
-                break;
-            case 8:
-                cout << "Tam";
-                break;
-            case 9:
-                cout << "Chin";
-                break;
-            default:
-                break;
-            }
+        case 2:
+            cout << "Hai";
+            break;
+        case 3:
+            cout << "Ba";
+            break;
+        case 4:
+            cout << "Bon";
+            break;
+        case 5:
+            cout << "Nam";
+            break;
+        case 6:
+            cout << "Sau";
+            break;
+        case 7:
+            cout << "Bay";
+            break;
+        case 8:
+            cout << "Tam";
+            break;
+        case 9:
+            cout << "Chin";
+            break;
+        default:
+            break;
+        }
 
-            if (n / 10 == 1)
-            {
-                cout << "Muoi ";
-            }
-            else
-                cout << " muoi ";
-            // units
-            switch (n % 10)
-            {
-            case 1:
-                cout << "mot";
-                break;
-            case 2:
-                cout << "hai";
-                break;
-            case 3:
-                cout << "ba";
-                break;
-            case 4:
-                cout << "bon";
-                break;
-            case 5:
-                cout << "lam";
-                break;
-            case 6:
-                cout << "sau";
-                break;
-            case 7:
-                cout << "bay";
-                break;
-            case 8:
-                cout << "tam";
-                break;
-            case 9:
-                cout << "chin";
-                break;
-            default:
-                break;
-            }
+        if (n / 10 == 1)
+        {
+            cout << "Muoi ";
         }
         else
+            cout << " muoi ";
+        // units
+        switch (n % 10)
         {
-            switch (n)
-            {
-            case 1:
-                cout << "Mot";
-                break;
-            case 2:
-                cout << "Hai";
-                break;
-            case 3:
-                cout << "Ba";
-                break;
-            case 4:
-                cout << "Bon";
-                break;
-            case 5:
-                cout << "Nam";
-                break;
-            case 6:
-                cout << "Sau";
-                break;
-            case 7:
-                cout << "Bay";
-                break;
-            case 8:
-                cout << "Tam";
-                break;
-            case 9:
-                cout << "Chin";
-                break;
-            default:
-                break;
-            }
+        case 1:
+            cout << "mot";
+            break;
+        case 2:
+            cout << "hai";
+            break;
+        case 3:
+            cout << "ba";
+            break;
+        case 4:
+            cout << "bon";
+            break;
+        case 5:
+            cout << "lam";
+            break;
+        case 6:
+            cout << "sau";
+            break;
+        case 7:
+            cout << "bay";
+            break;
+        case 8:
+            cout << "tam";
+            break;
+        case 9:
+            cout << "chin";
+            break;
+        default:
+            break;
         }
-        cout << endl;
+    }
+    else // 0 < n <= 9
+    {
+        switch (n)
+        {
+        case 1:
+            cout << "Mot";
+            break;
+        case 2:
+            cout << "Hai";
+            break;
+        case 3:
+            cout << "Ba";
+            break;
+        case 4:
+            cout << "Bon";
+            break;
+        case 5:
+            cout << "Nam";
+            break;
+        case 6:
+            cout << "Sau";
+            break;
+        case 7:
+            cout << "Bay";
+            break;
+        case 8:
+            cout << "Tam";
+            break;
+        case 9:
+            cout << "Chin";
+            break;
+        default:
+            break;
+        }
     }
 
     return 0;
