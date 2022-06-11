4354e07873830a2ecae45dceab818dc94462485c
NguyenThinhDatEng <toithienamxinnghe@gmail.com>
2022-06-11 17:15:40 +0700

    accomplish chapter 5

---
+++ C++/Thanh_thao_C++_qua_108_bai_hoc/Chuong_5/Bai_41-Khai_bao_mang.cpp
@@ -0,0 +1,13 @@
+#include <iostream> // sizeof(int)
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    // declare arrays
+    int a[18];
+    char c[] = {'a', 'b', 'c'};
+    // initialize the number of elements of the array first
+    int b = 3;
+    int d[b];
+    return 0;
+}
