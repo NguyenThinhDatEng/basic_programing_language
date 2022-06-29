a27e678934e463183462321a8b7abfef92e06098
NguyenThinhDatEng <toithienamxinnghe@gmail.com>
2022-06-29 08:35:02 +0700

    plt

--- python/libary/pd.py
+++
@@ -1,2 +0,0 @@
-import pandas as pd
-# read_csv, head, shape, info, describe, access, compare; return array, len; create column, drop, plot
--- python/libary/plt.py
+++
@@ -1,2 +0,0 @@
-import matplotlib.pyplot as plt # draw graph
-# pylot, plot, show, title, xlabel, ylabel, figure, subplot, legend, bar, histogram, scatter
